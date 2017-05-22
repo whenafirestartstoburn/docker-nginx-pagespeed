8d727b51c90fcc8dda12dbf9f85d3da913f07d0f
Mark Hilton <mark@crunchgeek.com>
2017-05-22 17:17:36 -0400

    relaxed WordPress security for a while

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,9 +8,4 @@ if [[ "$1" == -* ]]; then
     set -- nginx -g daemon off; "$@"
 fi
 
-if [ -n "$PAGESPEED_ENABLE" ]
-then
-    sed -i 's|pagespeed on;|pagespeed '"$PAGESPEED_ENABLE"';|g' /etc/nginx/include/pagespeed.conf
-fi
-
 exec "$@"
